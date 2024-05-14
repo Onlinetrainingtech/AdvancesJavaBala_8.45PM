bc07de7c9ce8931dcef775b094c343c73cce942a
azartechnet <azartechnet@gmail.com>
2024-05-14 22:08:04 +0530

    updated

---
+++ HibernateRelationshipOne_One/src/com/example/Store.java
@@ -0,0 +1,66 @@
+package com.example;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class Store {
+
+	public static void main(String[] args) {
+		
+		StandardServiceRegistry ssr=new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
+		Metadata meta=new MetadataSources(ssr).getMetadataBuilder().build();
+		SessionFactory factory=meta.getSessionFactoryBuilder().build();
+		Session session=factory.openSession();
+		Transaction t=session.beginTransaction();
+		
+		Employee e1=new Employee();
+		e1.setName("mohamed");
+		e1.setEmail("mohamed@gmail.com");
+		
+		/*Address address1=new Address();
+		address1.setAddressLine1("karur");
+		address1.setAddressId(1001);
+		address1.setCity("krr");
+		address1.setCountry("India");
+		address1.setState("TN");
+		address1.setPincode(12345);
+		
+		e1.setAddress(address1);
+		
+		address1.setEmployee(e1);
+		
+		session.persist(e1);
+		
+		t.commit();
+		
+		session.close();
+		
+		System.out.println("Inserted..");*/
+		
+		Address address1=new Address();
+		address1.setAddressLine1("karur");
+		address1.setCity("india");
+		address1.setState("Tn");
+		address1.setCountry("India");
+		address1.setPincode(10001);
+		
+		e1.setAddress(address1);
+
+		address1.setEmployee(e1);
+		
+		session.persist(e1);
+		t.commit();
+		
+		session.close();
+		
+		System.out.println("Inserted..");
+		
+		
+	}
+
+}
