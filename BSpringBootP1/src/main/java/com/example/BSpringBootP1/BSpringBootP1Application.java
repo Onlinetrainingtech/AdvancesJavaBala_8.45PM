ece6cfd485627ddd134dcd98fc6cec9c523665b1
azartechnet <azartechnet@gmail.com>
2024-06-25 07:45:11 +0530

    updated

---
+++ BSpringBootP1/src/main/java/com/example/BSpringBootP1/BSpringBootP1Application.java
@@ -0,0 +1,13 @@
+package com.example.BSpringBootP1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BSpringBootP1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BSpringBootP1Application.class, args);
+	}
+
+}
