1aa7ff50e16303d8551cc9a68603dae2918feba5
azartechnet <azartechnet@gmail.com>
2025-04-15 11:49:15 +0530

    updated

---
+++ DemoP1/src/main/java/com/example/DemoP1/DemoP1Application.java
@@ -0,0 +1,13 @@
+package com.example.DemoP1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoP1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoP1Application.class, args);
+	}
+
+}
---
+++ DemoP1/src/test/java/com/example/DemoP1/DemoP1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DemoP1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoP1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
